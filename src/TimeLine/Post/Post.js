b33c7984e8eeeeacbce52ceb7aeaab949cc8eefc
Yogesh Saini <yogeshsaini9700@gmail.com>
2023-07-17 13:13:51 +0530

    frontend part is completed

--- src/TimeLine/Post/Post.js
+++ src/TimeLine/Post/Post.js
@@ -4,18 +4,18 @@ import Avatar from '@mui/material/Avatar';
 import { FavoriteBorderOutlined,ChatOutlined,SendOutlined,BookmarkBorder } from '@mui/icons-material';
 import './Post.css'
 
-function Post() {
+function Post({Name,Img,Timeline,Likes}) {
   return (
     <div className='post'>
         <div className='post__header'>
            <div className='post__headerAuther'>
            <Avatar>H</Avatar>
-            Virat Kohil • <span> 10h </span>
+            {Name}  • <span>  {Timeline} </span>
            </div>
            <MoreHoriz />
         </div>
         <div className='post__image'>
-          <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg' alt='postimage' />
+          <img  src={Img} alt='postimage' />
         </div>
         <div className='post__footer'>
             <div className='post__footericons'>
@@ -29,7 +29,7 @@ function Post() {
             </div>
             </div>
         </div>
-       Like By 21 People.
+       Like By {Likes} People.
     </div>
   )
 }
--- src/TimeLine/Suggestion.js
+++ src/TimeLine/Suggestion.js
@@ -1,8 +1,84 @@
 import React from 'react'
+import './Suggestion.css'
+import { Avatar } from '@mui/material'
 
 function Suggestion() {
   return (
-    <div>Suggestion</div>
+    <div className='suggestion'>
+      <div className='suggestion__title'>Suggestion For You</div>
+      <div className='suggestion__usernames'>
+        <div className='suggestion__username'>
+          <div className='user__left'>
+              <div className='username__avatar'>
+                <Avatar>D</Avatar>
+              </div> 
+              <div className='username__info'>
+                <h5>Dhoni</h5>
+                <h6>New To Instagram</h6>
+              </div>
+          </div>
+          <div className='user__right'>
+             <button className='user__btn'>Follow</button>
+          </div>
+        </div>
+        <div className='suggestion__username'>
+          <div className='user__left'>
+              <div className='username__avatar'>
+                <Avatar>D</Avatar>
+              </div> 
+              <div className='username__info'>
+                <h5>Dhoni</h5>
+                <h6>New To Instagram</h6>
+              </div>
+          </div>
+          <div className='user__right'>
+             <button className='user__btn'>Follow</button>
+          </div>
+        </div>
+        <div className='suggestion__username'>
+          <div className='user__left'>
+              <div className='username__avatar'>
+                <Avatar>D</Avatar>
+              </div> 
+              <div className='username__info'>
+                <h5>Dhoni</h5>
+                <h6>New To Instagram</h6>
+              </div>
+          </div>
+          <div className='user__right'>
+             <button className='user__btn'>Follow</button>
+          </div>
+        </div>
+        <div className='suggestion__username'>
+          <div className='user__left'>
+              <div className='username__avatar'>
+                <Avatar>D</Avatar>
+              </div> 
+              <div className='username__info'>
+                <h5>Dhoni</h5>
+                <h6>New To Instagram</h6>
+              </div>
+          </div>
+          <div className='user__right'>
+             <button className='user__btn'>Follow</button>
+          </div>
+        </div>
+        <div className='suggestion__username'>
+          <div className='user__left'>
+              <div className='username__avatar'>
+                <Avatar>D</Avatar>
+              </div> 
+              <div className='username__info'>
+                <h5>Dhoni</h5>
+                <h6>New To Instagram</h6>
+              </div>
+          </div>
+          <div className='user__right'>
+             <button className='user__btn'>Follow</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
